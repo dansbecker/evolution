6ba16a6aae51fd6a3a4e22e5a8a6894e30fd9f9a
Dan Becker <dansbecker@gmail.com>
2018-03-13 11:55:45 -0600

    Add random-move-player as opponent

--- examples/grid_search.py
+++ examples/grid_search.py
@@ -6,11 +6,13 @@ from keras.models import Model
 from keras.layers import Dense, Flatten, Input
 
 from numpy import mean, inf
+from random import shuffle
+import pandas as pd
 
 
+example_dims = [3,3]
 
-
-def get_example_model(board_dims=[3,3], n_players=2, hidden_size=20):
+def get_example_model(board_dims=example_dims, n_players=2, hidden_size=20):
     input_board = Input(board_dims + [n_players])
     flattened_input = Flatten()(input_board)
     hidden_layer_1 = Dense(hidden_size, activation='relu')(flattened_input)
@@ -27,21 +29,43 @@ def first_open_square_player():
                     return (i, j)
         # all squares are full
         return (0, 0)
-    return StrategyFunctionPlayer(board_dims=(3,3), move_fn=first_open_square_strat)
+    return StrategyFunctionPlayer(board_dims=example_dims, move_fn=first_open_square_strat)
+
+def random_open_square_player():
+    def random_open_square_strat(board):
+        moves = [(x, y) for x in range(board.shape[0]) for y in range(board.shape[1])]
+        shuffle(moves)
+        for i, j in moves:
+            if (board[i, j, :] == 0).all():
+                return (i, j)
+
+        # all squares are full
+        return (0, 0)
+    return StrategyFunctionPlayer(board_dims=example_dims, move_fn=random_open_square_strat)
 
 if __name__ == "__main__":
-    for lr in [0.1]:
-        for rand_level in [0.01, 0.1]:
-            for hidden_size in [20]:
-                TicTacToeMaker = GameMaker(TicTacToe, board_dims=(3, 3))
+    n_generations = 50
+    n_agents = 50
+    games_per_matchup = 20
+    max_possible_score = games_per_matchup
+
+    results = []
+    for lr in [0.5, 1, 2]:
+        for rand_level in [0.1, 0.5]:
+            for hidden_size in [25]:
+                TicTacToeMaker = GameMaker(TicTacToe, board_dims=example_dims)
                 my_model = get_example_model(hidden_size=hidden_size)
-                my_pop = Population(100, model=my_model, update_method='grad',
+                my_pop = Population(n_agents, model=my_model, update_method='grad',
                                     game_maker=TicTacToeMaker, evolution_lr=lr, rand_level=rand_level)
-                for i in range(51):
-                    my_pop.score_and_evolve(opponents=[first_open_square_player()], games_per_matchup=10)
-                    if ((i % 25) == 0):
-                        print("LR: " + str(lr) + "\t rand level: " + str(rand_level) + "\t hidden size: " + str(hidden_size) +
-                          "\t Generations: " +str(i) + "\t Mean Recent Score: " + str(my_pop.prev_gen_scores.mean()) +
-                          "\t Best Recent Score: " + str(my_pop.prev_gen_scores.max())
-                          )
+                for i in range(n_generations):
+                    if my_pop.prev_gen_scores is None or my_pop.prev_gen_scores.mean() < max_possible_score:
+                        my_pop.score_and_evolve(opponents=[random_open_square_player()],
+                                                games_per_matchup=games_per_matchup)
+
+                current_results = {'lr': lr, 'rand_level': rand_level, 'hidden_size': hidden_size,
+                                           'generations': my_pop.generation_num, 'mean_pop_score': my_pop.prev_gen_scores.mean(),
+                                           'best_score_in_pop': my_pop.prev_gen_scores.max()}
+                print(current_results)
+                results.append(current_results)
+    pd.DataFrame(results).to_csv('./optimization_results.csv', index=False)
     print('Done')
